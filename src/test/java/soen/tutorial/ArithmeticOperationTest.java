7778cfcc577e574c949cadf1c86e334887b87e47
melisa1026 <68923209+melisa1026@users.noreply.github.com>
2022-11-30 18:43:13 -0500

    Update ArithmeticOperationTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -17,7 +17,7 @@ public class ArithmeticOperationTest {
 		assertEquals(expected, actual);
 
 	}
-    /*
+    
     @Test
     public void testSub()
     {
@@ -28,6 +28,5 @@ public class ArithmeticOperationTest {
         assertEquals(expected, actual);
 
     }
-    */
 
 }
